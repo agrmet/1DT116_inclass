b39d9d6a8c347f90d0072727597e521b3ca3ce08
agrmet <agron-metaj@hotmail.com>
2025-08-07 15:06:47 +0200

    isPrime done

--- IRL2/isPrime.c
+++ IRL2/isPrime.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <omp.h>
+#include <math.h>
 
 int N = 50000000;
 
 //If is prime, returns 1; else returns 0
 int isPrime(int n) {
-    //Fill me!
+    int limit = sqrt(n);
+    for (int i = 2; i <= limit; i++)
+    {
+        if(n % i == 0){
+            return 0;
+        } 
+    }
+
     return 1;
 }
 
@@ -16,7 +24,11 @@ int main()
 
     //Task: find the total number of prime numbers in the range [2, N]
     begin = omp_get_wtime();
-    //Fill me! (Call isPrime)
+    #pragma omp parallel for reduction(+:countPrimes)
+    for (int i = 2; i < N; i++)
+    {
+        countPrimes += isPrime(i);
+    }
     end = omp_get_wtime();
     printf("Found %d prime numbers between 1 and %d\n", countPrimes, N);
     printf("Took %lfs to find primes\n", end-begin);
