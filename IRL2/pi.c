bb1a953431306c6e9158c19d58b505d6802d9279
agrmet <agron-metaj@hotmail.com>
2025-08-11 08:37:49 +0200

    pi done

--- IRL2/pi.c
+++ IRL2/pi.c
@@ -4,19 +4,26 @@
 double step;
 static long num_steps = 100000000L;
 
-int main (){
-    long i;
-    double x, pi = 0.0, sum=0.0;
-    step = 1.0/(double) num_steps; // step number of very small bars!
+int main() {
+    double pi = 0.0;
+    step = 1.0 / (double)num_steps; // step number of very small bars!
     double T1, T2;
 
     T1 = omp_get_wtime();
-    //FILL ME
-    //For each small bar (x = 0 to num_steps) calculate the y value of this small bar.
-    //Then multiply the y values with the total number of steps to get the area (integral)
+
+    #pragma omp parallel default(none) shared(num_steps, step) reduction(+:pi)
+    {
+        double x=0; 
+    
+        #pragma omp for
+        for (long i = 0; i < num_steps; i++) {
+            x = (i + 0.5) * step;
+            pi += (4.0 / (1 + x * x)) * step;
+        }
+    }
 
     T2 = omp_get_wtime();
 
-    printf("Time: %lf, pi: %lf\n", T2-T1, pi);
+    printf("Time: %lf, pi: %lf\n", T2 - T1, pi);
     return 0;
 }
